47d410252d86830999cd6f913f590f821cae058b
andreas <andreas.isenegger@bitcontrol.ch>
2023-12-06 20:44:42 +0100

    Add FB19Exa2_Ctrl amd FB19Exa2_Subs examples

---
+++ exa/STM32CubeIDE/STM32F401/2nodes/FB19Exa2_Ctrl/Core/Inc/FB19MsgQ.h
@@ -0,0 +1,66 @@
+/**
+ *  - Filename:     FB19MsgQ.h
+ *  - Platform(s):  All
+ *  - Project:      FieldBus19
+ *  - Created:      Apr 21, 2023
+ *  - Description:  This file publishes the interface of the FieldBus19 message
+ *                  queue.
+ *  - Notes:        DON'T CHANGE IT!
+ *  - Author:       Andreas Isenegger
+ *  - Copyright:    2023, Bitcontrol GmbH, Switzerland.
+ *                  All rights reserved.
+ *  @file
+ *  @brief          This file publishes the interface of the FieldBus19 message
+ *                  queue.
+ */
+
+#ifndef FB19MSGQ_H_
+#define FB19MSGQ_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "FB19Def.h"
+#include "FB19Msg.h"
+
+//------------------------------------------------------------------------------
+// Symbols and Macros
+
+//------------------------------------------------------------------------------
+// Types
+/**
+ * FieldBus19 message queue type.
+ */
+typedef struct FB19MsgQ_struct
+{
+    /** A pointer to the array that stores the elements of the queue. */
+    FB19Msg_t* array;
+
+    /** The capacity of the queue (number of messages in the array). */
+    const short capacity;
+} FB19MsgQ_t;
+
+//------------------------------------------------------------------------------
+// Constants and Variables
+
+//------------------------------------------------------------------------------
+// Functions
+/**
+ * This function is used to initialize a message queue.
+ * All messages in the queue will be be initialized with \c 0, including ALL
+ * their internal data structures.
+ * It is no error, if the capacity of the queue is 0; in that case no
+ * operation will be performed on the queue. If the capacity is a negative
+ * number however, an error will be returned.
+ *
+ * @param[in] msg points to the message queue that shall be initialized.
+ * @return \c R_SUCCESS if the parameter checks pass, \c R_ERROR otherwise.
+ */
+int FB19MsgQ_init(FB19MsgQ_t* queue);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // FB19MSGQ_H_
---
+++ exa/STM32CubeIDE/STM32F401/2nodes/FB19Exa2_Ctrl/Core/Src/FB19CtrlCfg.c
@@ -0,0 +1,104 @@
+/*
+ *  Filename:       FB19CtrlCfg.c
+ *  Platform(s):    STM32F401x
+ *  Project:        <Your project name>
+ *  Created:        November 29, 2023
+ *  Description:    This file implements the interface of the FieldBus19
+ *                  Controller configuration module.
+ *                  Add it to the project directory and adjust it to the
+ *                  requirements of the application.
+ *  Notes:
+ *  Author:         Andreas Isenegger
+ *  Copyright:      2023, Bitcontrol GmbH, Switzerland.
+ *                  All rights reserved.
+ */
+
+#include <stm32f4xx.h> // For GPIOA, USART2, ...
+
+#include "FB19Ctrl.h"
+#include "FB19CtrlCfg.h"
+
+//------------------------------------------------------------------------------
+// Symbols and Macros
+
+//------------------------------------------------------------------------------
+// Types
+
+//------------------------------------------------------------------------------
+// Constants and Variables
+
+/*
+ * FB19 Controller
+ * ===============
+ */
+/* FB19 Controller Data Preparation low frequency reader message queue. */
+static FB19LibMsgQ_element_t sDprCtrlLfrArray[4];
+static FB19LibMsgQ_queue_t sDprCtrlLfrMsgQ =
+    { array: sDprCtrlLfrArray, capacity: sizeof(sDprCtrlLfrArray) / sizeof(FB19LibMsgQ_element_t) };
+
+/* FB19 Controller Data Preparation receive message queue. */
+static FB19Msg_t sDprCtrlRxArray[8];
+static FB19MsgQ_t sDprCtrlRxMsgQ =
+    { array: sDprCtrlRxArray, capacity: sizeof(sDprCtrlRxArray) / sizeof(FB19Msg_t) };
+
+/* FB19 Controller Data Preparation transmit message queue. */
+static FB19Msg_t sDprCtrlTxArray[8];
+static FB19MsgQ_t sDprCtrlTxMsgQ =
+    { array: sDprCtrlTxArray, capacity: sizeof(sDprCtrlTxArray) / sizeof(FB19Msg_t) };
+
+/* FB19 Controller Driver TxRx message queue. */
+static FB19Msg_t sDrvCtrlTxRxArray[8];
+static FB19MsgQ_t sDrvCtrlTxRxMsgQ =
+    { array: sDrvCtrlTxRxArray, capacity: sizeof(sDrvCtrlTxRxArray) / sizeof(FB19Msg_t) };
+
+FB19Ctrl_cfg_t cfgFB19Ctrl =        // FB19 Controller configuration
+{
+    /* FB19 Controller Data Preparation Module. *******************************/
+    {
+        &sDprCtrlLfrMsgQ,           // Low frequency reader message queue
+        &sDprCtrlRxMsgQ,            // Receive message queue
+        &sDprCtrlTxMsgQ,            // Transmit message queue
+    },
+
+    /* FB19 Controller Driver Module. *****************************************/
+    {
+        GPIOA,                      // CMSIS GPIO base address
+        8,                          // GPIO_portBitDOE
+        &sDrvCtrlTxRxMsgQ,          // Transmit/receive message queue
+    },
+
+    /* Timer Driver Module. ***************************************************/
+    {
+        TIM2_IRQn,                  // NVIC_IRQ_channel
+        2,                          // NVIC_IRQ_groupPriority
+        3,                          // NVIC_IRQ_subPriority
+        1UL << 0,                   // RCC_clockEnableBitMask
+        &(RCC->APB1ENR),            // RCC_clockEnableRegister
+        1UL << 0,                   // RCC_resetBitMask
+        &(RCC->APB1RSTR),           // RCC_resetRegister
+        32,                         // registerWidth [bit]
+        TIM2,                       // TIMx
+    },
+
+    /* UART Driver Module. ****************************************************/
+    {
+        GPIOA,                      // CMSIS GPIO base address
+        7,                          // GPIO_alternateFunctionSelection
+        3,                          // GPIO_portBitNumberRx
+        2,                          // GPIO_portBitNumberTx
+        USART2_IRQn,                // NVIC_IRQ_channel
+        2,                          // NVIC_IRQ_groupPriority
+        3,                          // NVIC_IRQ_subPriority
+        RCC_APB1ENR_USART2EN,       // RCC_clockEnableBitMask
+        &(RCC->APB1ENR),            // RCC_clockEnableRegister
+        USART2,                     // CMSIS USART base address
+    },
+};
+
+FB19Ctrl_inst_t instFB19Ctrl;       // FB19 Controller memory and instance
+
+//------------------------------------------------------------------------------
+// Local Functions
+
+//------------------------------------------------------------------------------
+// Global Functions
